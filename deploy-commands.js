a121a5c83241fdd542feef11c00860b8a46f6002
강은찬 <99608927+Xeron0310@users.noreply.github.com>
2022-05-12 08:23:11 +0900

    patch 5

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,41 +1,20 @@
-const fs = require("fs");
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
-const { clientId, guildId, token } = require("./config.json");
+const fs = require("fs")
+//require('dotenv').config()
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+const {token, clientId, guildId} = require('./config.json')
 
 const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: "9" }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(token);
 
-(async () => {
-  guildId.map(async (guildId) => {
-    try {
-      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-        body: commands,
-      });
-      console.log(`${guildId} 서버 성공`);
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
-  // 글로벌 명령어 등록 시 주석을 풀고 사용
-  // try {
-  //   await rest.put(Routes.applicationCommands(clientId), {
-  //     body: commands,
-  //   });
-  //   console.log(`글로벌 명령어 등록 성공`);
-  // } catch (error) {
-  //   console.error(error);
-  // }
-})();
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
--- index.js
+++ index.js
@@ -3,37 +3,31 @@ const { token } = require("./config.json");
 const fs = require("fs");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
 client.commands = new Collection();
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.data.name, command);
 }
 
-client.once("ready", () => {
-  console.log("서버 준비 완료!");
+client.once('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("interactionCreate", async (interaction) => {
+client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
 
-  if (!command) return;
+	if (!command) return;
 
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: '명령어를 실행하는 데 오류가 발생했습니다.', ephemeral: true });
+	}
+})
 
 client.login(token);
